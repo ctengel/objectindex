6d22d11e523f693826b3b84e8974021764aa5921
Chris Engelhart <engleman@gmail.com>
2023-07-06 08:42:37 -0400

    Added more info to upload
    Part of #11
    Not yet tested

--- obj_idx/client.py
+++ obj_idx/client.py
@@ -3,19 +3,46 @@
 Currently it relies on ObjectIndex for all info it needs on S3, other than bucket name
 """
 
+import socket
+import pathlib
+import hashlib
+import mimetypes
 from . import s3lib, clilib
+
+SW_STRING = 'OIC-0.1'
+BLOCK_SIZE = 16777216
+
+def checksum(file_path: pathlib.Path) -> bytes:
+    """Get SHA256 checksum of a given path"""
+    check = hashlib.sha256()
+    with open(file_path, "rb") as file_obj:
+        while True:
+            data = file_obj.read(BLOCK_SIZE)
+            if len(data) == 0:
+                break
+            check.update(data)
+    return check.digest()
+
+def get_mime(file_path: pathlib.Path) -> str:
+    """Determine MIME type of a given path"""
+    # TODO add magic from mediacrawler
+    return mimetypes.guess_type(file_path)[0]
 
 def upload(filename: str, obj_idx: clilib.ObjectIndex, bucket: str, tags: dict):
     """Run an actual file upload into ObjIdx and S3"""
-    # TODO calculate filename, ignoring prefix
-    # TODO calculate url, likely file://[hostname?]/path
-    # TODO gather data on file... obj_size, mtime, checksum, mime
-    my_file = obj_idx.initiate_upload(url=None,
+    # TODO consider refactoring information gathering with mediacrawler fs.File.get_media()
+    file_path = pathlib.Path(filename)
+    file_stat = file_path.stat()
+    file_checksum = checksum(file_path)
+    file_mime = get_mime(file_path)
+    my_file = obj_idx.initiate_upload(url=f"file://{socket.gethostname()}/{file_path.absolute()}",
                                       bucket=bucket,
-                                      obj_size=None,
-                                      mtime=None,
-                                      filename=None,
-                                      extra_file=tags)
+                                      obj_size=file_stat.st_size,
+                                      mtime=file_stat.st_mtime,
+                                      filename=file_path.name,
+                                      extra_file=tags,
+                                      checksum=file_checksum,
+                                      mime=file_mime)
     if not my_file.exists():
         s3_url = my_file.get_s3_url()
         # TODO send checksum
@@ -26,5 +53,5 @@ def upload(filename: str, obj_idx: clilib.ObjectIndex, bucket: str, tags: dict):
 
 def get_obj_idx(url):
     """Get ObjectIndex object"""
-    # TODO add in host, user, sw
-    return clilib.ObjectIndex(url)
+    # TODO add in user and auth
+    return clilib.ObjectIndex(url, host=socket.gethostname(), sw=SW_STRING)
