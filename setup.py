2027b12675ad66be06dbce523421adf0e6d3be04
Chris Engelhart <engleman@gmail.com>
2023-08-21 20:27:36 -0400

    Various packaging
    Part of #46

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Basic setup.py file
+
+The bulk of all info is in pyproject.toml
+This is needed to be able to do pip install -e
+
+pip 21.3 and/or PEP 660 should eliminate this need
+"""
+from setuptools import setup
+
+setup()
