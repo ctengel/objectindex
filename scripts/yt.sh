72ae23baf6d45d642f54f9892188173cff313f17
Chris Engelhart <engleman@gmail.com>
2025-10-26 05:38:55 -0400

    bugfix

--- scripts/yt.sh
+++ scripts/yt.sh
@@ -11,5 +11,5 @@ shift
 for url in "$@"; do
 	yt-dlp --restrict-filenames --write-info-json --download-archive "$archive" "$url"
 	"$mypath/yt.py" -b "$bucket" -l "$lpm" *.info.json
-	obj-idx-client --rm *.mp4
+	obj-idx-client check --rm *.mp4
 done
